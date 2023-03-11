b5500e11b7a6b65bc94cdfa1285e0eda6e9f2b16
Denis Burdin <dionisiy.god.of.wine@gmail.com>
2023-03-12 00:43:41 +0200

    init

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,18 @@
+
+import s from './Button.module.css';
+
+const Button = ({ handleButtonClick }) => {
+  return (
+    <div className={s.boxBtn}>
+      <button
+        type="button"
+        className={s.loadmoreBtn}
+        onClick={() => handleButtonClick()}
+      >
+        Load more
+      </button>
+    </div>
+  );
+};
+
+export default Button;
